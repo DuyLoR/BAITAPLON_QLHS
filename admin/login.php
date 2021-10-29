dfe42a202b4034601e8ecc24ff082254b7692d71
loofeht <phu832001@gmail.com>
2021-10-29 12:26:34 +0700

    sửa lại tất cả giao diện

--- admin/login.php
+++ admin/login.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <link rel="stylesheet" href="assets/css/login.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
     <title>Quản lý học sinh</title>
 </head>
 
@@ -36,7 +36,43 @@
 
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="assets/js/login.js"></script>
+    <!-- <script src="assets/js/login.js"></script> -->
+    <script>
+        $(document).ready(function() {
+            $('.btn-login').click(function() {
+                $userName = $('#userName').val();
+                $password = $('#password').val();
+
+                if ($userName == '' || $password == '') {
+                    alert("Vui lòng nhập đầy đủ TK và MK");
+                } else {
+                    $.ajax({
+                        url: "process-login.php",
+                        method: "POST",
+                        data: {
+                            do_login: "do_login",
+                            userName: $userName,
+                            password: $password
+                        },
+                        success: function(response) {
+                            if (response == "admin") {
+                                window.location.href = "admin-index.php";
+                            } else if (response == "teacher") {
+                                window.location.href = "teacher-index.php";
+                            } else if (response == "wrong") {
+                                alert("Mật khẩu không chính xác !");
+                            } else {
+                                alert("Tài khoản không tồn tại")
+                            }
+                        }
+                    });
+
+
+                }
+
+            });
+        })
+    </script>
 </body>
 
 </html>
