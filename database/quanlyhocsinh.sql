aac52fe63c33f4b0c4032147f749512c33c776b5
DuyLoR <duya7k50@gmail.com>
2021-10-29 21:29:40 +0700

    Chỉnh sửa lại cơ sở dữ liệu

--- database/quanlyhocsinh.sql
+++ database/quanlyhocsinh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 28, 2021 at 05:22 PM
+-- Generation Time: Oct 29, 2021 at 04:28 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -28,22 +28,20 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `dangnhap` (
-  `id` int(11) NOT NULL,
-  `hoten` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id` int(225) NOT NULL,
   `tendangnhap` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `matkhau` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `quantrivien` tinyint(1) DEFAULT 0
+  `capdo` tinyint(2) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `dangnhap`
 --
 
-INSERT INTO `dangnhap` (`id`, `hoten`, `tendangnhap`, `matkhau`, `quantrivien`) VALUES
-(1, 'Nguyễn Khương Duy', 'admin', 'admin', 1),
-(2, 'Nguyễn Văn phú', 'admin1', 'admin1', 1),
-(3, 'Bùi Thị Khánh Linh', 'admin2', 'admin2', 1),
-(4, 'Nguyễn Văn A', 'abc', 'abc', 0);
+INSERT INTO `dangnhap` (`id`, `tendangnhap`, `matkhau`, `capdo`) VALUES
+(1, 'admin', 'admin', 1),
+(2, 'giaovien', 'giaovien', 2),
+(3, 'hocsinh', 'hocsinh', 0);
 
 -- --------------------------------------------------------
 
@@ -54,19 +52,21 @@ INSERT INTO `dangnhap` (`id`, `hoten`, `tendangnhap`, `matkhau`, `quantrivien`) 
 CREATE TABLE `diem` (
   `mahs` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `mamh` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hocky` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hocky` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `diemm` float NOT NULL,
   `diemgk` float NOT NULL,
-  `diemthi` float NOT NULL
+  `diemck` float NOT NULL,
+  `diemtb` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `diem`
 --
 
-INSERT INTO `diem` (`mahs`, `mamh`, `hocky`, `diemgk`, `diemthi`) VALUES
-('HS00', 'MH00', 'Học kỳ 1', 10, 10),
-('HS01', 'MH00', 'Học kỳ 1', 9, 9),
-('HS02', 'MH00', 'Học kỳ 1', 5, 0);
+INSERT INTO `diem` (`mahs`, `mamh`, `hocky`, `diemm`, `diemgk`, `diemck`, `diemtb`) VALUES
+('HS00', 'MH00', 'Học kỳ 1 2021-2022', 10, 10, 10, 10),
+('HS01', 'MH00', 'Học kỳ 1 2021-2022', 9, 9, 9, 9),
+('HS02', 'MH00', 'Học kỳ 1 2021-2022', 9, 9, 9, 9);
 
 -- --------------------------------------------------------
 
@@ -79,7 +79,7 @@ CREATE TABLE `giaovien` (
   `tengv` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gioitinh` tinyint(1) NOT NULL,
   `chucvu` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `sodt` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sodt` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `diachi` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -90,8 +90,7 @@ CREATE TABLE `giaovien` (
 
 INSERT INTO `giaovien` (`magv`, `tengv`, `gioitinh`, `chucvu`, `sodt`, `email`, `diachi`) VALUES
 ('GV00', 'Nguyễn Văn A', 1, 'Giáo Viên', '1234567891', 'anv@gmail.com', 'Hà Nội'),
-('GV01', 'Nguyễn Văn B', 1, 'Giáo Viên', '1234567892', 'bnv@gmail.com', 'Hà Nam'),
-('GV02', 'Nguyễn Văn C', 1, 'Phó hiệu trưởng', '1234567893', 'cnv@gmail.com', 'Hà Nội');
+('GV01', 'Nguyễn Văn B', 1, 'Phó hiệu trưởng', '1234567892', 'bnv@gmail.com', 'Hà Nội');
 
 -- --------------------------------------------------------
 
@@ -101,9 +100,10 @@ INSERT INTO `giaovien` (`magv`, `tengv`, `gioitinh`, `chucvu`, `sodt`, `email`, 
 
 CREATE TABLE `hocsinh` (
   `mahs` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tenhs` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `malop` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hoten` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `gioitinh` tinyint(1) NOT NULL,
+  `maph` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `diachi` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `khoahoc` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -112,52 +112,10 @@ CREATE TABLE `hocsinh` (
 -- Dumping data for table `hocsinh`
 --
 
-INSERT INTO `hocsinh` (`mahs`, `tenhs`, `malop`, `gioitinh`, `diachi`, `khoahoc`) VALUES
-('HS00', 'Nguyễn Văn P', '10A1', 1, 'Hà Nội', '2020-2023'),
-('HS01', 'Nguyễn Văn H', '10A1', 1, 'Hà Nội', '2020-2023'),
-('HS02', 'Nguyễn Văn K', '10A1', 1, 'Hà Nội', '2020-2023');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `khenthuong`
---
-
-CREATE TABLE `khenthuong` (
-  `makt` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mahs` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hocky` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ngaykt` date NOT NULL,
-  `ghichu` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `khenthuong`
---
-
-INSERT INTO `khenthuong` (`makt`, `mahs`, `hocky`, `ngaykt`, `ghichu`) VALUES
-('KT00', 'HS00', 'Học kỳ 1', '2021-10-28', 'Học giỏi ');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kiluat`
---
-
-CREATE TABLE `kiluat` (
-  `makl` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mahs` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `hocki` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ngaykl` date NOT NULL,
-  `ghichu` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `kiluat`
---
-
-INSERT INTO `kiluat` (`makl`, `mahs`, `hocki`, `ngaykl`, `ghichu`) VALUES
-('KL00', 'HS02', 'Học kỳ 1', '2021-10-28', 'Bỏ thi');
+INSERT INTO `hocsinh` (`mahs`, `malop`, `hoten`, `gioitinh`, `maph`, `diachi`, `khoahoc`) VALUES
+('HS00', 'LOP0', 'Nguyễn Khương Duy', 1, 'PH00', 'Hà Nội', '2020-2023'),
+('HS01', 'LOP0', 'Nguyễn Văn phú', 1, 'PH01', 'Hà Nội', '2020-2023'),
+('HS02', 'LOP0', 'Bùi Thị Khánh Linh', 0, 'PH02', 'Hà Nội', '2020-2023');
 
 -- --------------------------------------------------------
 
@@ -167,7 +125,7 @@ INSERT INTO `kiluat` (`makl`, `mahs`, `hocki`, `ngaykl`, `ghichu`) VALUES
 
 CREATE TABLE `lop` (
   `malop` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tenlop` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tenlop` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `magvcn` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -176,8 +134,8 @@ CREATE TABLE `lop` (
 --
 
 INSERT INTO `lop` (`malop`, `tenlop`, `magvcn`) VALUES
-('10A1', '10A1', 'GV01'),
-('10A2', '10A2', 'GV00');
+('LOP0', '10A1', 'GV00'),
+('LOP1', '10A2', 'GV01');
 
 -- --------------------------------------------------------
 
@@ -187,20 +145,41 @@ INSERT INTO `lop` (`malop`, `tenlop`, `magvcn`) VALUES
 
 CREATE TABLE `monhoc` (
   `mamh` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tenmh` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `sotiet` int(225) NOT NULL,
-  `magv` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+  `magv` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tenmh` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sotiet` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `monhoc`
 --
 
-INSERT INTO `monhoc` (`mamh`, `tenmh`, `sotiet`, `magv`) VALUES
-('MH00', 'Toán', 45, 'GV00'),
-('MH00', 'Toán', 45, 'GV01'),
-('MH01', 'Lý', 45, 'GV00'),
-('MH02', 'Hóa', 45, 'GV00');
+INSERT INTO `monhoc` (`mamh`, `magv`, `tenmh`, `sotiet`) VALUES
+('MH00', 'GV00', 'Toán', 45),
+('MH01', 'GV00', 'Lý', 45),
+('MH02', 'GV00', 'Hóa', 45);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `phuhuynh`
+--
+
+CREATE TABLE `phuhuynh` (
+  `maph` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tenph` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sodt` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `phuhuynh`
+--
+
+INSERT INTO `phuhuynh` (`maph`, `tenph`, `sodt`, `email`) VALUES
+('PH00', 'Phụ Huynh A', '1234567881', 'aph@gmail.com'),
+('PH01', 'Phụ Huynh B', '1234567882', 'bph@gmail.com'),
+('PH02', 'Phụ Huynh C', '1234567883', 'cph@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -229,36 +208,29 @@ ALTER TABLE `giaovien`
 -- Indexes for table `hocsinh`
 --
 ALTER TABLE `hocsinh`
-  ADD PRIMARY KEY (`mahs`,`malop`),
-  ADD KEY `malop` (`malop`);
-
---
--- Indexes for table `khenthuong`
---
-ALTER TABLE `khenthuong`
-  ADD PRIMARY KEY (`makt`,`mahs`),
-  ADD KEY `mahs` (`mahs`);
-
---
--- Indexes for table `kiluat`
---
-ALTER TABLE `kiluat`
-  ADD PRIMARY KEY (`makl`,`mahs`),
-  ADD KEY `mahs` (`mahs`);
+  ADD PRIMARY KEY (`mahs`),
+  ADD KEY `malop` (`malop`),
+  ADD KEY `maph` (`maph`);
 
 --
 -- Indexes for table `lop`
 --
 ALTER TABLE `lop`
-  ADD PRIMARY KEY (`malop`,`magvcn`),
+  ADD PRIMARY KEY (`malop`),
   ADD KEY `magvcn` (`magvcn`);
 
 --
 -- Indexes for table `monhoc`
 --
 ALTER TABLE `monhoc`
-  ADD PRIMARY KEY (`mamh`,`magv`),
+  ADD PRIMARY KEY (`mamh`),
   ADD KEY `magv` (`magv`);
+
+--
+-- Indexes for table `phuhuynh`
+--
+ALTER TABLE `phuhuynh`
+  ADD PRIMARY KEY (`maph`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -268,7 +240,7 @@ ALTER TABLE `monhoc`
 -- AUTO_INCREMENT for table `dangnhap`
 --
 ALTER TABLE `dangnhap`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
@@ -278,26 +250,15 @@ ALTER TABLE `dangnhap`
 -- Constraints for table `diem`
 --
 ALTER TABLE `diem`
-  ADD CONSTRAINT `diem_ibfk_1` FOREIGN KEY (`mahs`) REFERENCES `hocsinh` (`mahs`),
-  ADD CONSTRAINT `diem_ibfk_2` FOREIGN KEY (`mamh`) REFERENCES `monhoc` (`mamh`);
+  ADD CONSTRAINT `diem_ibfk_1` FOREIGN KEY (`mamh`) REFERENCES `monhoc` (`mamh`),
+  ADD CONSTRAINT `diem_ibfk_2` FOREIGN KEY (`mahs`) REFERENCES `hocsinh` (`mahs`);
 
 --
 -- Constraints for table `hocsinh`
 --
 ALTER TABLE `hocsinh`
-  ADD CONSTRAINT `hocsinh_ibfk_1` FOREIGN KEY (`malop`) REFERENCES `lop` (`malop`);
-
---
--- Constraints for table `khenthuong`
---
-ALTER TABLE `khenthuong`
-  ADD CONSTRAINT `khenthuong_ibfk_1` FOREIGN KEY (`mahs`) REFERENCES `hocsinh` (`mahs`);
-
---
--- Constraints for table `kiluat`
---
-ALTER TABLE `kiluat`
-  ADD CONSTRAINT `kiluat_ibfk_1` FOREIGN KEY (`mahs`) REFERENCES `hocsinh` (`mahs`);
+  ADD CONSTRAINT `hocsinh_ibfk_1` FOREIGN KEY (`malop`) REFERENCES `lop` (`malop`),
+  ADD CONSTRAINT `hocsinh_ibfk_2` FOREIGN KEY (`maph`) REFERENCES `phuhuynh` (`maph`);
 
 --
 -- Constraints for table `lop`
