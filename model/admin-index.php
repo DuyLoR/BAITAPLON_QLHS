41bbff362ba305e1dbfb74800e28862fe332c61c
DuyLoR <duya7k50@gmail.com>
2021-10-29 22:44:48 +0700

    Chinh sửa

--- model/admin-index.php
+++ model/admin-index.php
@@ -18,7 +18,7 @@
 
 <body>
 
-    <body id="body-pd">
+    <body id="body-pd" class="bg-light">
         <header class="header" id="header">
             <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
             <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
