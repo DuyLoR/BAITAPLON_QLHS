1bdda9aaf2707a77ef020bb9493d3d232a3540eb
loofeht <phu832001@gmail.com>
2021-10-31 12:19:15 +0700

    lấy lại dữ liệu

---
+++ process/process-login.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (isset($_POST['do_login'])) { //phải bấm đăng nhập thì mới vào được trang này
+    $userName = $_POST['userName'];
+    $password = $_POST['password'];
+
+    include '../config/config.php';
+    $sql = "SELECT * FROM dangnhap WHERE tendangnhap = '$userName'";
+    $result = mysqli_query($conn, $sql);
+
+    //Xác thực
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        // $pass_hash = $row['user_pass'];
+        $level = $row['capdo'];
+        // if (password_verify($password, $pass_hash))
+        if ($password == $row['matkhau']) {
+            $_SESSION['currentUser'] = $userName;
+            $_SESSION['currentLevel'] = $level;
+
+            if ($level == 1) { //Kiểm tra user level
+                echo "admin";  //admin
+            } else if ($level == 2) {
+                echo "teacher"; //teacher
+            } else {
+                echo "student";
+            }
+        } else {
+            echo "wrong"; //sai password
+        }
+    } else {
+        echo "fail"; //sai username
+    }
+} else header('location:login.php'); //chuyển về trang đăng nhập
--- process/process-logout.php
+++ process/process-logout.php
@@ -2,3 +2,4 @@
 session_start();
 unset($_SESSION['currentUser']);
 unset($_SESSION['currentLevel']);
+header('location:../model/login.php');
