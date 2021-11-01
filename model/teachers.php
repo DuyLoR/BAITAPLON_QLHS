8e8f994621d45bc28949a8018db3a57264c9392f
khingm12 <khanhlinhthibui10x@gmail.com>
2021-11-01 10:15:05 +0700

    thêm xóa giáo viên

--- model/teachers.php
+++ model/teachers.php
@@ -85,7 +85,7 @@ session_start();
                                             <a href="#" class="btn btn-primary btn-flat manage_class">
                                                 <i class="fas fa-edit"></i>
                                             </a>
-                                            <button type="button" class="btn btn-danger btn-flat delete_class">
+                                            <button name="<?php echo $row['magv'];  ?>" class="btn btn-danger btn-flat deleteTeacher">
                                                 <i class="fas fa-trash"></i>
                                             </button>
                                         </div>
@@ -115,5 +115,30 @@ session_start();
         $('.newTeacher').click(function() {
             $('#contents').load("add-teacher.php")
         })
+
+
+         //? xoá lớp 
+         $('.deleteTeacher').click(function() {
+            $id = $(this).attr('name'); //? bắt giá trị name của hàng
+            if (confirm("Bạn có muốn xoá lớp '" + $id + "' không?")) {
+                //? nếu đồng ý
+                $.ajax({
+                    type: "post",
+                    url: "../process/process-delete-teachers.php",
+                    data: {
+                        teacherId: $id,
+                    },
+                    success: function(response) {
+                        if (response == 'success') {
+                            alert("Xoá thành công!")
+                            location.reload()
+                        } else if (response == 'error') {
+                            alert("Xoá thất bại")
+                        }
+                    }
+                });
+            } else return false;
+
+        });
     })
 </script>
---
+++ process/process-delete-teacher.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_POST['teacherId'])) {
+    $classId = $_POST['teacherId'];
+
+    //?mở kết nối
+    include '../config/config.php';
+
+    //? set câu lệnh truy vấn
+    $sql = "DELETE FROM giaovien WHERE magv='$teacherId'";
+
+    //? kiểm tra và thực thi câu lệnh
+    if (mysqli_query($conn, $sql)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+    //? đóng kết nối
+    mysqli_close($conn);
+}
