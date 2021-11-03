64a4acf85de9547503bd24be0feb9ab6c06f29f9
khingm12 <khanhlinhthibui10x@gmail.com>
2021-11-03 11:33:14 +0700

    Thêm dashboard

--- model/dashboard.php
+++ model/dashboard.php
@@ -1,43 +1,66 @@
 <?php
-session_start();
+    session_start();
+    if (isset($_SESSION['currentUser'])) {
 ?>
-<div class="main-cards">
-    <div class="card">
-        <i class="fa fa-user-o fa-2x text-lightblue"></i>
-        <div class="card-inner">
-            <p class="text-primary-p">Khoa</p>
-            <span class="font-bold text-title">10</span>
+<?php
+    if ($_SESSION['currentLevel'] == 1) {
+?>
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-start">Trang chủ</h1>
+            </div>
         </div>
+            <hr class="border-primary">
     </div>
-    <div class="card">
-        <i class="fa fa-user-o fa-2x text-lightblue"></i>
-        <div class="card-inner">
-            <p class="text-primary-p">Lớp</p>
-            <span class="font-bold text-title">10</span>
         </div>
-    </div>
-    <div class="card">
-        <i class="fa fa-user-o fa-2x text-lightblue"></i>
-        <div class="card-inner">
-            <p class="text-primary-p">Sinh viên</p>
-            <span class="font-bold text-title">10</span>
+        <div class="height-100 bg-light">
+            <div class="col py-3 ">
+                <div class="container">
+                    <div class="row mt-3 justify-content-around py-5 mb-3">
+                        <div class="col-md-3 col-12 bg-info d-flex mb-2 rounded justify-content-center">
+                            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-user-graduate"></i></div>
+                            <div class="ps-3 my-3">
+                                <p>Tổng số học sinh</p>
+                                <p></p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-12 bg-info d-flex mb-2 rounded justify-content-center">
+                            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-user-tie"></i></div>
+                            <div class="ps-3 my-3">
+                                <p>Tổng số giáo viên</p>
+                                <p></p>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-12 bg-info d-flex mb-2 rounded justify-content-center">
+                            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-book-open"></i></div>
+                            <div class="ps-3 my-3">
+                                <p>Tổng số môn học</p>
+                                <p></p>
+                            </div>
+                        </div>
+                    </div>
+
+            <div class="row mt-3 justify-content-around py-5">
+                        <div class="col-md-3 col-12 bg-info d-flex mb-2 rounded justify-content-center">
+                            <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-school"></i></div>
+                            <div class="ps-3 my-3">
+                                <p>Tổng số lớp học</p>
+                                <p></p>
+                            </div>
+            </div>
         </div>
+
     </div>
-    <div class="card">
-        <i class="fa fa-user-o fa-2x text-lightblue"></i>
-        <div class="card-inner">
-            <p class="text-primary-p">Môn học</p>
-            <span class="font-bold text-title">10</span>
-        </div>
-    </div>
-    <div class="card">
-        <i class="fa fa-user-o fa-2x text-lightblue"></i>
-        <div class="card-inner">
-            <p class="text-primary-p">Giảng viên</p>
-            <span class="font-bold text-title">10</span>
-        </div>
-    </div>
+
 </div>
+        <?php
+    }
+        ?>
+    <?php
+} else header('location:login.php');
+    ?>
 <?php
     include 'footer.php';
 ?>
