227da6d1aba063f82ca1bbb52d4b0c26e4341b9d
khingm12 <khanhlinhthibui10x@gmail.com>
2021-11-03 11:58:10 +0700

    Thêm dashboard

--- model/dashboard.php
+++ model/dashboard.php
@@ -1,24 +1,22 @@
 <?php
-    session_start();
-    if (isset($_SESSION['currentUser'])) {
-?>
-<?php
+session_start();
+if (isset($_SESSION['currentUser'])) {
     if ($_SESSION['currentLevel'] == 1) {
 ?>
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0 text-start">Trang chủ</h1>
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-start">Trang chủ</h1>
+                    </div>
+                </div>
+                <hr class="border-primary">
             </div>
-        </div>
-            <hr class="border-primary">
-    </div>
         </div>
         <div class="height-100 bg-light">
             <div class="col py-3 ">
                 <div class="container">
-                    <div class="row mt-3 justify-content-around py-5 mb-3">
+                    <div class="row mt-3 justify-content-around py-5 ">
                         <div class="col-md-3 col-12 bg-info d-flex mb-2 rounded justify-content-center">
                             <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-user-graduate"></i></div>
                             <div class="ps-3 my-3">
@@ -42,25 +40,18 @@
                         </div>
                     </div>
 
-            <div class="row mt-3 justify-content-around py-5">
+                    <div class="row justify-content-around py-5">
                         <div class="col-md-3 col-12 bg-info d-flex mb-2 rounded justify-content-center">
                             <div class="mt-3 mb-3" style="font-size: 1.875rem"><i class="fas fa-school"></i></div>
                             <div class="ps-3 my-3">
                                 <p>Tổng số lớp học</p>
                                 <p></p>
                             </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-
-    </div>
-
-</div>
-        <?php
-    }
-        ?>
     <?php
+    }
 } else header('location:login.php');
     ?>
-<?php
-    include 'footer.php';
-?>
