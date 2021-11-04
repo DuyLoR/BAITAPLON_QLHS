cbab9fb7a8ab8936c89f858d81b615c2cdd67359
DuyLoR <duya7k50@gmail.com>
2021-11-04 11:43:55 +0700

    Sửa lỗi edit account

--- process/process-add-account.php
+++ process/process-add-account.php
@@ -1,33 +1,34 @@
 <?php
-$userName = $_POST['userName'];
-$password = $_POST['password'];
-$level = $_POST['level'];
-$studentId = "NULL";
-$teacherId = "NULL";
-include '../config/config.php';
+    $userName = $_POST['userName'];
+    $password = $_POST['password'];
+    $level = $_POST['level'];
+    $studentId = "NULL";
+    $teacherId = "NULL";
 
-$pass_hash = password_hash($password, PASSWORD_DEFAULT);
+    include '../config/config.php';
 
-$sql = "INSERT INTO `dangnhap`(tendangnhap, matkhau, capdo, mahs, magv) 
-    VALUES ('$userName' ,'$pass_hash' ,'$level', $studentId, $teacherId )";
-if ($level == 0) {
-    $studentId = $_POST['studentId'];
+    $pass_hash = password_hash($password, PASSWORD_DEFAULT);
+
     $sql = "INSERT INTO `dangnhap`(tendangnhap, matkhau, capdo, mahs, magv) 
-    VALUES ('$userName' ,'$pass_hash' ,'$level', '$studentId', $teacherId )";
-} else if ($level == 2) {
-    $teacherId = $_POST['teacherId'];
-    $sql = "INSERT INTO `dangnhap`(tendangnhap, matkhau, capdo, mahs, magv) 
-    VALUES ('$userName' ,'$pass_hash' ,'$level', $studentId, '$teacherId' )";
-}
+        VALUES ('$userName' ,'$pass_hash' ,'$level', $studentId, $teacherId )";
+    if ($level == 0) {
+        $studentId = $_POST['studentId'];
+        $sql = "INSERT INTO `dangnhap`(tendangnhap, matkhau, capdo, mahs, magv) 
+        VALUES ('$userName' ,'$pass_hash' ,'$level', '$studentId', $teacherId )";
+    } else if ($level == 2) {
+        $teacherId = $_POST['teacherId'];
+        $sql = "INSERT INTO `dangnhap`(tendangnhap, matkhau, capdo, mahs, magv) 
+        VALUES ('$userName' ,'$pass_hash' ,'$level', $studentId, '$teacherId' )";
+    }
 
 
-$result = mysqli_query($conn, $sql);
-if ($result > 0) {
-    echo "success";
-} else {
-    echo "error";
-}
-?>
+    $result = mysqli_query($conn, $sql);
+    if ($result > 0) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+    ?>
 <?php
     //? đóng kết nối
     mysqli_close($conn);
