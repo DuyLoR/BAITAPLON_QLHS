941849e3c5bd9ab75fab95addef9057a9139b96f
loofeht <phu832001@gmail.com>
2021-11-04 11:33:28 +0700

    lược bỏ css

--- index.php
+++ index.php
@@ -66,36 +66,7 @@ if (isset($_SESSION['currentUser'])) {
         <!--Container Main end-->
 
         <!-- chat -->
-        <div class="l_c_h">
-            <div class="c_h">
-                <div class="left_c">
-                    <div class="left right_c left_icons">
-                        <a href="#" class="mini" style="font-size:23px;">+</a>
-                    </div>
-                    <div class="left center_icons">
-                        <!--center_icons-->
-                        Chat for help!
-                    </div>
-                    <!--end center_icons-->
-                </div>
-                <div class="right right_c" style="width:35px;">
-                    <a href="#" class="logout" title="End chat" name="" style="display:none;"><img src="chat/logout.png"></a>
-                </div>
-                <div class="clear"></div>
-            </div>
-            <div class="chat_container" style="display: none;">
-                <p class="no_provider">
-                    Xin chào <?php echo $_SESSION['currentUser'] ?>! tôi có thể giúp gì cho bạn?
-                </p>
-
-                <div class="chat_entry">
-                    <div class="form-floating">
-                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 200px"></textarea>
-                    </div>
-                    <input type="submit" name="chat_submit" class="chat_submit" id="chat_submit" value="Gửi">
-                </div>
-            </div>
-        </div>
+        <?php include './model/chat.php' ?>
 
         <!-- footer -->
         <?php include_once './model/footer.php' ?>
