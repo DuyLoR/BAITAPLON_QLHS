890d005c25d8dc7fde6aa9931aac23996390ccdd
DuyLoR <duya7k50@gmail.com>
2021-11-04 11:07:09 +0700

    Thêm điều kiện nhập dữ liệu input

--- model/add-subject.php
+++ model/add-subject.php
@@ -2,33 +2,35 @@
 session_start();
 if ($_SESSION['currentLevel'] == 1 || $_SESSION['currentLevel'] == 2) {
 ?>
-    <div class="container-fluid">
-        <h2 class="border-bottom border-info p-1">Thêm môn học</h2>
-        <div class="col-lg-12">
-            <div class="card card-outline card-primary border-2 border-info mt-4">
-                <div class="card-body ">
-                    <form action="" id="manage-student">
-                        <input type="hidden" name="id" value="">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="form-group text-dark">
-                                    <div class="form-group">
-                                        <label for="" class="control-label p-1 mt-1">Mã môn học#</label>
-                                        <input type="text" class="form-control form-control-sm" id="subjectID" value="" required>
-                                    </div>
+<div class="container-fluid">
+    <h2 class="border-bottom border-info p-1">Thêm môn học</h2>
+    <div class="col-lg-12">
+        <div class="card card-outline card-primary border-2 border-info mt-4">
+            <div class="card-body ">
+                <form action="" id="manage-student">
+                    <input type="hidden" name="id" value="">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group text-dark">
+                                <div class="form-group">
+                                    <label for="" class="control-label p-1 mt-1">Mã môn học#</label>
+                                    <input type="text" class="form-control form-control-sm" id="subjectID" value=""
+                                        required>
                                 </div>
-                                <div class="form-group text-dark">
-                                    <div class="form-group">
-                                        <label for="" class="control-label p-1 mt-1">Tên môn học</label>
-                                        <input type="text" class="form-control form-control-sm" id="subjectName" value="" required>
-                                    </div>
+                            </div>
+                            <div class="form-group text-dark">
+                                <div class="form-group">
+                                    <label for="" class="control-label p-1 mt-1">Tên môn học</label>
+                                    <input type="text" class="form-control form-control-sm" id="subjectName" value=""
+                                        required>
                                 </div>
-                                <div class="form-group text-dark">
-                                    <div class="form-group">
-                                        <label for="" class="control-label p-1 mt-1">Giáo viên</label>
-                                        <select id="teacherID" id="" class="form-select form-select-sm " required>
-                                            <!-- Lấy dữ liệu từ database -->
-                                            <?php
+                            </div>
+                            <div class="form-group text-dark">
+                                <div class="form-group">
+                                    <label for="" class="control-label p-1 mt-1">Giáo viên</label>
+                                    <select id="teacherID" id="" class="form-select form-select-sm " required>
+                                        <!-- Lấy dữ liệu từ database -->
+                                        <?php
                                             //? mở kết nối
                                             include '../config/config.php';
                                             $sql = "SELECT * FROM giaovien";
@@ -40,76 +42,77 @@ if ($_SESSION['currentLevel'] == 1 || $_SESSION['currentLevel'] == 2) {
                                                 }
                                             }
                                             ?>
-                                        </select>
-                                    </div>
+                                    </select>
                                 </div>
+                            </div>
 
-                                <div class="form-group text-dark mb-3">
-                                    <div class="form-group">
-                                        <label for="" class="control-label p-1 mt-1">Số tiết</label>
-                                        <input type="text" class="form-control form-control-sm" id="subjectPrice" value="" required>
-                                    </div>
+                            <div class="form-group text-dark mb-3">
+                                <div class="form-group">
+                                    <label for="" class="control-label p-1 mt-1">Số tiết</label>
+                                    <input type="text" class="form-control form-control-sm" id="subjectPrice" value=""
+                                        required>
                                 </div>
                             </div>
                         </div>
-                        <div class="card-footer">
-                            <div class="d-flex w-100 justify-content-center align-items-center">
-                                <button class="btn btn-success  mx-2" type="submit" id="btnSubmit">Lưu</button>
-                                <a class="btn btn-danger mx-2 backSubject" href="#">Quay lại</a>
-                            </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="d-flex w-100 justify-content-center align-items-center">
+                            <button class="btn btn-success  mx-2" type="submit" id="btnSubmit">Lưu</button>
+                            <a class="btn btn-danger mx-2 backSubject" href="#">Quay lại</a>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 <?php
 } else header('location:login.php');
 ?>
 <script>
-    $(document).ready(function() {
-        $('.backSubject').click(function() {
-            $('#contents').load("./model/subjects.php")
-        })
-
-        $('#btnSubmit').click(function() {
-            $subjectID = $('#subjectID').val();
-            $subjectName = $('#subjectName').val();
-            $subjectPrice = $('#subjectPrice').val();
-            $teacherID = $('#teacherID').val();
-
-            if ($subjectID == "" || $subjectName == "" || $subjectPrice == "" || $teacherID == "") {
-                alert("Vui lòng nhập đủ thông tin");
-                return;
-            } else if($subjectID.length <4){
-                alert("Mã môn học ít nhất 4 ký tự!");
-                return;
-            }else if($subjectPrice.isNaN <20){
-                alert("Số tiết học cần lớn hơn 20!")
-                return;
-            }else{
-                $.ajax({
-                    type: "post",
-                    url: "./process/process-add-subject.php",
-                    data: {
-                        subjectName: $subjectName,
-                        subjectID: $subjectID,
-                        subjectPrice: $subjectPrice,
-                        teacherID: $teacherID,
-                    },
-                    success: function(response) {
-                        if (response == "success") {
-                            alert("Thêm thành công");
-                            $('#contents').load("./model/subjects.php");
-                        } else {
-                            alert("Thêm thất bại");
-                        }
-                    }
-                });
-            }
-
-        });
+$(document).ready(function() {
+    $('.backSubject').click(function() {
+        $('#contents').load("./model/subjects.php")
     })
+
+    $('#btnSubmit').click(function() {
+        $subjectID = $('#subjectID').val();
+        $subjectName = $('#subjectName').val();
+        $subjectPrice = $('#subjectPrice').val();
+        $teacherID = $('#teacherID').val();
+
+        if ($subjectID == "" || $subjectName == "" || $subjectPrice == "" || $teacherID == "") {
+            alert("Vui lòng nhập đủ thông tin");
+            return;
+        } else if ($subjectID.length < 4) {
+            alert("Mã môn học ít nhất 4 ký tự!");
+            return;
+        } else if (isNaN($subjectPrice)) {
+            alert("Số tiết học không bao gồm kí tự")
+            return;
+        }  else {
+            $.ajax({
+                type: "post",
+                url: "./process/process-add-subject.php",
+                data: {
+                    subjectName: $subjectName,
+                    subjectID: $subjectID,
+                    subjectPrice: $subjectPrice,
+                    teacherID: $teacherID,
+                },
+                success: function(response) {
+                    if (response == "success") {
+                        alert("Thêm thành công");
+                        $('#contents').load("./model/subjects.php");
+                    } else {
+                        alert("Thêm thất bại");
+                    }
+                }
+            });
+        }
+
+    });
+})
 </script>
 <?php
     //? đóng kết nối
